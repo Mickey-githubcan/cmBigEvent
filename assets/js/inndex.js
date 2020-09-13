945876199106394db55af535bc5fe701ff76f7ff
风清扬 <2896189494@qq.com>
2020-09-13 20:06:12 +0800

    js代码提交

--- assets/js/inndex.js
+++ assets/js/inndex.js
@@ -1,60 +1,63 @@
-// $(function () {
-//   // 获取用户信息
-//   // /my/userinfo /  header / ls中的token
+$(function () {
+    // 获取用户信息
+    // /my/userinfo /  header / ls中的token
 
-//   //   获取登录后保存在本地的token
-//   //   var token = window.localStorage.getItem('token') || ''
-//   fetUserInfo();
+    //   获取登录后保存在本地的token
+    //   var token = window.localStorage.getItem('token') || ''
+    fetUserInfo();
 
-//   function getUserInfo() {
-//     $.ajax({
-//       //后端接口
-//       url: "/my/userinfo",
-//       //请求头设置(s不要忘)
-//       // headers: {
-//       //     Authorization: DOMSettableTokenList,
-//       // },
-//       success: function (res) {
-//         // debugger (断点)
-//         // console.log(res);
-//         // res.data.user_pic
-//         console.log("success", res);
-//         if (res.status === 1) return;
-//         var resname = res.data.nickname || res.data.username;
-//         $("#welcome").html(resname);
+    function getUserInfo() {
+        $.ajax({
+            //后端接口
+            url: "/my/userinfo",
+            //请求头设置(s不要忘)
+            // headers: {
+            //     Authorization: DOMSettableTokenList,
+            // },
+            success: function (res) {
+                // debugger (断点)
+                // console.log(res);
+                // res.data.user_pic
+                console.log("success", res);
+                if (res.status === 1) return;
+                var resname = res.data.nickname || res.data.username;
+                $("#welcome").html(resname);
 
-//         //存在: 显示图片头像，隐藏文字头像
-//         if (res.data.user_pic) {
-//           $(".layui-nav-img").atter("src", res.data.user_pic).show();
-//           $(".text-avater").hide();
-//         } else {
-//           //反之
-//           $(".layui-nav-img").hide();
+                //存在: 显示图片头像，隐藏文字头像
+                if (res.data.user_pic) {
+                    $(".layui-nav-img").atter("src", res.data.user_pic).show();
+                    $(".text-avater").hide();
+                } else {
+                    //反之
+                    $(".layui-nav-img").hide();
 
-//           $(".text-avater").html(resname[0].toUpperCase());
-//         }
-//       },
-//     });
-//   }
+                    $(".text-avater").html(resname[0].toUpperCase());
+                }
+            },
+        });
+    }
 
-//   window.getUserInfo = getUserInfo;
+    window.getUserInfo = getUserInfo;
 
-//   //用户退出
-//   $("#btn-logout").click(function (e) {
-//     e.preventDefault();
+    //用户退出
+    $("#btn-logout").click(function (e) {
+        e.preventDefault();
 
-//     //确认框
-//     layui.layer.confirm("is not?", { icon: 3, title: "提示" }, function (
-//       index
-//     ) {
-//       //do something
-//       // 取消
-//       //  确定=>
-//       //   2. 清空token
-//       window.localStorage.removeItem("token");
-//       // 1. 跳转登录
-//       window.location.href = "/login.html";
-//       layer.close(index);
-//     });
-//   });
-// });
+        //确认框
+        layui.layer.confirm("is not?", {
+            icon: 3,
+            title: "提示"
+        }, function (
+            index
+        ) {
+            //do something
+            // 取消
+            //  确定=>
+            //   2. 清空token
+            window.localStorage.removeItem("token");
+            // 1. 跳转登录
+            window.location.href = "/login.html";
+            layer.close(index);
+        });
+    });
+});
